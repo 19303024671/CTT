02fa63d6ca2293f1c28415a0125ae064e32ec72a
whl <3074647498@qq.com>
2023-07-15 18:25:03 +0800

    画出扇形了嘿嘿

--- CCTLib.cpp
+++ CCTLib.cpp
@@ -51,7 +51,7 @@ int DrawCCT::BinToInt(const vector<int>& bin, int N)
 	int num = 0;
 	for (int i : bin)
 	{
-		num += i *pow(2,N-1);
+		num += i *int(pow(2,N-1));
 		N -= 1;	
 	}
 	return num;
@@ -59,7 +59,6 @@ int DrawCCT::BinToInt(const vector<int>& bin, int N)
 
 void DrawCCT::DrawACCT()
 {
-	double unit_angle = 360.0 / cct_infor.N;//单位角度
 	//创建文件夹
 	if (!fs::exists(cct_infor.dir_path))
 	{
@@ -86,15 +85,30 @@ void DrawCCT::DrawACCT()
 	switch (cct_infor.color)
 	{
 	case black:
-		image.setTo(cv::Scalar(0, 0, 0));
+		//设置画布颜色
+		image.setTo(cv::Scalar(255, 255, 255));
 		break;
 	case white:
-		image.setTo(cv::Scalar(255, 255, 255));
+		image.setTo(cv::Scalar(0, 0, 0));
 		break;
 	default:
 		break;
 	}
-	cv::imshow("画布展示：",image);
+	//绘制扇形
+	double unit_angle = 360.0 / cct_infor.N;//单位角度
+	int k = 0;
+	for (int i : cct_infor.bin)
+	{
+		if (i == 1) cv::ellipse(image,
+			cct_infor.flabellate.center,
+			cv::Size(cct_infor.flabellate.radius,
+				cct_infor.flabellate.radius),
+			0, k * unit_angle, (k + 1) * unit_angle,
+			cct_infor.flabellate.color,
+			cct_infor.flabellate.thickness);
+		k += 1;
+	}
+	cv::imshow("扇形绘制", image);
 	cv::waitKey(0);
 }
 
@@ -105,10 +119,16 @@ void CCTInfo::Init()
 	case black:
 		dir_path = string("./CCT_IMG_") + to_string(N)
 			+ string("_Black/");
+		flabellate.center = cv::Point(size / 2, size / 2);
+		flabellate.color = cv::Scalar(0, 0, 0);
+		flabellate.radius = int(0.4 * size);
 		break;
 	case white:
 		dir_path = string("./CCT_IMG_") + to_string(N)
 			+ string("_White/");
+		flabellate.center = cv::Point(size / 2, size / 2);
+		flabellate.color = cv::Scalar(255, 255, 255);
+		flabellate.radius = int(0.4 * size);
 		break;
 	default:
 		break;
@@ -116,7 +136,7 @@ void CCTInfo::Init()
 }
 
 CCTInfo::CCTInfo(const int& N_, const CCTColor& color_, 
-	const double& size_, const int& num_):
+	const int& size_, const int& num_):
 	N(N_),color(color_),size(size_),num(num_)
 {
 }
@@ -124,3 +144,4 @@ CCTInfo::CCTInfo(const int& N_, const CCTColor& color_,
 CCTInfo::CCTInfo()
 {
 }
+
--- CCTLib.h
+++ CCTLib.h
@@ -15,6 +15,16 @@ enum CCTColor
 	black = 1,
 	white = 0	
 };
+//扇形
+struct Flabellate
+{
+	cv::Point center;//圆心
+	int radius;//半径
+	double start_angle;//起始角度
+	double end_angle;//终止角度
+	int thickness = -1;//厚度，-1表示填充
+	cv::Scalar color;//颜色(BGR顺序)
+};
 
 //绘制环形码所需的信息
 struct CCTInfo
@@ -22,12 +32,14 @@ struct CCTInfo
 	string dir_path;//文件夹路径
 	int N;//等分数
 	CCTColor color;//环形码颜色
-	double size;//图片尺寸
+	int size;//图片尺寸
 	int num;//编码的整数
 	vector<int> bin;//编码的二进制vector
+	Flabellate flabellate;//扇形信息
+
 public:
 	void Init();
-	CCTInfo(const int&N_,const CCTColor&color_,const double&size_,const int&num);
+	CCTInfo(const int&N_,const CCTColor&color_,const int&size_,const int&num);
 	CCTInfo();
 };
 
