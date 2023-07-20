5a140b5e298d08bb238643252d3a06a3a9f78476
whl <3074647498@qq.com>
2023-07-20 18:12:27 +0800

    可以识别很成功，不过感觉需要加一个识别头

--- CCTLib.cpp
+++ CCTLib.cpp
@@ -7,220 +7,52 @@
 
 using namespace std;
 
-DrawCCT::DrawCCT(const CCTInfo& cct_info_)
+
+DetectCCTInfo::DetectCCTInfo(const string& file_path_,
+	const CCTColor& color_,const int&N_) :file_path(file_path_), color(color_),N(N_)
 {
-	this->cct_infor = cct_info_;
-	this->cct_infor.Init();
-	this->cct_infor.bin = IntToBin(cct_infor.num,
-		cct_infor.N);
-	//计算N位二进制表示的最大十进制
-	vector<int>max_bin;
-	for (int i = 0; i < cct_infor.N; i++)
-		max_bin.push_back(1);
-	this->cct_infor.max_num = BinToInt(max_bin, cct_infor.N);
 }
 
-DrawCCT::~DrawCCT()
+DetectCCTInfo::DetectCCTInfo(const DetectCCTInfo& detect_cct_info_)
 {
-}
-/*
-* @brief 整数转二进制vector
-* 
-* @param num 待转整数
-* @param N 转成的位数，不足用0补齐
-*/
-vector<int> DrawCCT::IntToBin(int num,const int& N)
-{
-	vector<int> bin;
-	for (size_t i = 0; i < N; i++)
-	{
-		if (num>0)
-		{
-			bin.push_back(num % 2);
-			num = num / 2;
-		}
-		else
-		{
-			bin.push_back(0);
-		}
-	}
-	reverse(bin.begin(), bin.end());
-	return bin;
-}
-/*
-* @brief 将二进制转成整数
-*/
-int DrawCCT::BinToInt(const vector<int>& bin, int N)
-{
-	if (bin.size() != N) return 0;
-	int num = 0;
-	for (int i : bin)
-	{
-		num += i *int(pow(2,N-1));
-		N -= 1;	
-	}
-	return num;
+	this->color = detect_cct_info_.color;
+	this->file_path = detect_cct_info_.file_path;
+	this->N = detect_cct_info_.N;
 }
 
-cv::Mat DrawCCT::DrawACCT()
+DetectCCTInfo::DetectCCTInfo()
 {
+	this->color = black;
+	this->file_path = "";
+	this->N = 0;
+}
+
+cv::Mat DrawACCT(const ACCTInfo& acct_info)
+{
+	string dir_path = "";
+	string file_path = "";
 	//创建绘图画布
-	cv::Mat image(cct_infor.size, cct_infor.size, CV_8UC3);
-	switch (cct_infor.color)
+	cv::Mat image(acct_info.size, acct_info.size, CV_8UC3);
+	Circle circlel1(cv::Point(acct_info.size / 2, acct_info.size / 2), 0.2 * acct_info.size, 1, cv::Scalar(0,0,0));//内圆周
+	Circle circle_in_large(circlel1.center, 0.2 * acct_info.size, -1, circlel1.color);//里面的大圆
+	switch (acct_info.color)
 	{
 	case black:
+		file_path = string("E:/source/repos/main/CCT_Black_") + to_string(acct_info.N) + string("_/")
+			+ to_string(acct_info.num) + string(".png");
+		dir_path = string("E:/source/repos/main/CCT_Black_") + to_string(acct_info.N) + string("_/");
 		//设置画布颜色
 		image.setTo(cv::Scalar(255, 255, 255));
+		circlel1.color = cv::Scalar(0, 0, 0);
+		circle_in_large.color = cv::Scalar(255, 255, 255);
 		break;
 	case white:
+		file_path = string("E:/source/repos/main/CCT_White_") + to_string(acct_info.N) + string("_/")
+			+ to_string(acct_info.num) + string(".png");
+		dir_path = string("E:/source/repos/main/CCT_White_") + to_string(acct_info.N) + string("_/");
 		image.setTo(cv::Scalar(0, 0, 0));
-		break;
-	default:
-		break;
-	}
-	//绘制一个圆周
-	Circle circlel1 = cct_infor.circle_in;
-	circlel1.thickness = 1;
-	circlel1.radius = cct_infor.flabellate.radius;
-	cv::circle(image, circlel1.center, circlel1.radius,
-		circlel1.color, circlel1.thickness);
-	//绘制扇形
-	double unit_angle = 360.0 / cct_infor.N;//单位角度
-	int k = 0;
-	for (int i : cct_infor.bin)
-	{
-		if (i == 1) cv::ellipse(image,
-			cct_infor.flabellate.center,
-			cv::Size(cct_infor.flabellate.radius,
-				cct_infor.flabellate.radius),
-			0, k * unit_angle, (k + 1) * unit_angle,
-			cct_infor.flabellate.color,
-			cct_infor.flabellate.thickness);
-		k += 1;
-	}
-	//绘制内大圆
-	cv::circle(image, cct_infor.circle_out.center,
-		cct_infor.circle_out.radius,
-		cct_infor.circle_out.color,
-		cct_infor.circle_out.thickness);
-	//再绘制一个圆周
-	Circle circlel2 = cct_infor.circle_in;
-	circlel2.thickness = 1;
-	circlel2.radius = cct_infor.circle_out.radius;
-	cv::circle(image, circlel2.center, circlel2.radius,
-		circlel2.color, circlel2.thickness);
-	//绘制内小圆
-	cv::circle(image, cct_infor.circle_in.center,
-		cct_infor.circle_in.radius,
-		cct_infor.circle_in.color,
-		cct_infor.circle_in.thickness);
-	return image;
-}
-
-void DrawCCT::DrawaCCT()
-{
-	string file_name = to_string(cct_infor.num) +
-		string(".png");
-	string file_path = cct_infor.dir_path + file_name;
-	//保存绘制的图片
-	cv::imwrite(file_path, DrawACCT());
-}
-
-vector<cv::Mat> DrawCCT::DrawcCTs()
-{
-	vector<cv::Mat> images;
-	for (int i = 0; i < cct_infor.max_num; i++)
-	{
-		CCTInfo cct_info_temp = cct_infor;
-		cct_info_temp.num = i;
-		cct_info_temp.Init();
-		DrawCCT draw_cct_temp(cct_info_temp);
-		images.push_back(draw_cct_temp.DrawACCT());
-	}
-	return images;
-}
-
-void DrawCCT::DrawCCTs()
-{
-	//创建文件路径vector
-	vector<string> file_paths;
-	for (int i = 0; i < cct_infor.max_num; i++)
-	{
-		string file_name = to_string(i) +
-			string(".png");
-		string file_path = cct_infor.dir_path + file_name;
-		file_paths.push_back(file_path);
-	}
-	//循环绘制所有图片
-	vector<cv::Mat>images = DrawcCTs();
-	int k = 0;
-	for (auto& file_path : file_paths)
-	{
-		cv::imwrite(file_path, images[k]);
-		k += 1;
-	}
-}
-
-void DrawCCT::DrawCCTsOnAPic()
-{
-	vector<cv::Mat> imgs = DrawcCTs();
-	int pic_rows = int(sqrt(cct_infor.max_num)) + 1;
-	int pic_cols = pic_rows;
-	for (const auto& img : imgs)
-	{
-		if (img.empty())
-		{
-			cerr << "图片加载失败！" << endl;
-			return;
-		}
-	}
-	int pic_width = pic_cols * cct_infor.size;
-	int pic_height = pic_rows * cct_infor.size;
-	cv::Mat merged_img(pic_height, pic_width,
-		imgs[0].type());
-	int offsetX = 0;
-	int offsetY = 0;
-	for (const auto& img : imgs)
-	{
-		img.copyTo(merged_img(cv::Rect(offsetX, offsetY,
-			img.cols, img.rows)));
-		offsetX += img.cols;
-		if (offsetX >= pic_width)
-		{
-			offsetY += img.rows;
-			offsetX = 0;
-		}
-	}
-	string merged_img_path = cct_infor.dir_path +
-		string("merged_img_") + to_string(
-			cct_infor.max_num) + string(".png");
-	cout<<(cv::imwrite(merged_img_path, merged_img));
-	return;
-}
-
-void CCTInfo::Init()
-{
-	flabellate.center = cv::Point(size / 2, size / 2);
-	flabellate.radius = int(0.4 * size);
-	circle_in.center = cv::Point(size / 2, size / 2);
-	circle_in.radius = int(0.1 * size);
-	circle_out.center = cv::Point(size / 2, size / 2);
-	circle_out.radius = int(0.2 * size);
-	switch (color)
-	{
-	case black:
-		dir_path = string("E:/source/repos/main/CCT_IMG_") + to_string(N)
-			+ string("_Black/");	
-		flabellate.color = cv::Scalar(0, 0, 0);
-		circle_in.color = cv::Scalar(0, 0, 0);
-		circle_out.color = cv::Scalar(255, 255, 255);
-		break;
-	case white:
-		dir_path = string("E:/source/repos/main/CCT_IMG_") + to_string(N)
-			+ string("_White/");
-		flabellate.color = cv::Scalar(255, 255, 255);
-		circle_in.color = cv::Scalar(255, 255, 255);
-		circle_out.color = cv::Scalar(0, 0, 0);
+		circlel1.color = cv::Scalar(255, 255, 255);
+		circle_in_large.color = cv::Scalar(0, 0, 0);
 		break;
 	default:
 		break;
@@ -232,72 +64,59 @@ void CCTInfo::Init()
 		{
 			cerr << "创建文件夹：" << dir_path <<
 				"失败！" << endl;
-			return;
+			return cv::Mat();
 		}
 	}
-}
-
-CCTInfo::CCTInfo(const int& N_, const CCTColor& color_, 
-	const int& size_, const int& num_):
-	N(N_),color(color_),size(size_),num(num_)
-{
-}
-
-CCTInfo::CCTInfo()
-{
-}
-
-void DetectCCTInfo::Init()
-{
-	for (const auto& file_path :
-		fs::directory_iterator(dir_path))
+	//绘制扇形
+	Flabellate flabellate(circlel1.center, 0.4*acct_info.size, -1, circlel1.color);
+	double unit_angle = 360.0 / acct_info.N;//单位角度
+	int k = 0;
+	vector<int> bin = IntToBin(acct_info.num,acct_info.N);
+	for (int i : bin)
 	{
-		if (fs::is_regular_file(file_path))
-		{
-			fs::path file(file_path);
-			string s = file.extension().string();
-			if (file.extension().string() == ".jpg" ||
-				file.extension().string() == ".JPG" ||
-				file.extension().string() == ".png" ||
-				file.extension().string() == ".PNG")
-			{
-				img_file_paths.push_back(file_path.
-					path().string());
-			}
-		}
+		if (i == 1) cv::ellipse(image,
+			flabellate.center,
+			cv::Size(flabellate.radius,
+				flabellate.radius),
+			0, k * unit_angle, (k + 1) * unit_angle,
+			flabellate.color,
+			flabellate.thickness);
+		k += 1;
 	}
+	//绘制内大圆
+	cv::circle(image, circle_in_large.center,
+		circle_in_large.radius,
+		circle_in_large.color,
+		circle_in_large.thickness);
+	//绘制里面的圆周
+	cv::circle(image, circlel1.center, circlel1.radius,
+		circlel1.color, circlel1.thickness);
+	//再绘制外面的圆周
+	Circle circlel2(circlel1.center, 0.4 * acct_info.size, 1, circlel1.color);
+	cv::circle(image, circlel2.center, circlel2.radius,
+		circlel2.color, circlel2.thickness);
+	//绘制内小圆
+	Circle circle_in_small(circlel1.center, 0.1*acct_info.size, -1, circlel1.color);
+	cv::circle(image, circle_in_small.center,
+		circle_in_small.radius,
+		circle_in_small.color,
+		circle_in_small.thickness);
+	cv::imwrite(file_path, image);
+	return image;
 }
 
-DetectCCTInfo::DetectCCTInfo(const string& dir_path_,
-	const CCTColor& color_,const int&N_) :dir_path(dir_path_), color(color_),N(N_)
+vector<int> DecodeCCT(const DetectCCTInfo& detect_cct_info)
 {
-}
-
-DetectCCTInfo::DetectCCTInfo()
-{
-}
-
-DetectCCT::DetectCCT(const DetectCCTInfo& detect_cct_info_ )
-{
-	detect_cct_info = detect_cct_info_;
-	detect_cct_info.Init();
-}
-
-DetectCCT::~DetectCCT()
-{
-}
-
-vector<int> DetectCCT::DetectCCTsOnAPic()
-{
+	//读取图片
 	cv::Mat color_img = cv::imread(
-		detect_cct_info.img_file_paths[1]);
+		detect_cct_info.file_path);
 	if (color_img.empty())
 	{
-		cerr << "图片：" << detect_cct_info.
-			img_file_paths[0]
+		cerr << "图片：" << detect_cct_info.file_path
 			<< "加载失败！" << endl;
-		return vector<int>(-1);
+		return vector<int>( - 1);
 	}
+	//图片前处理
 	cv::Mat gray_img;
 	cv::cvtColor(color_img, gray_img,
 		cv::COLOR_BGR2GRAY);
@@ -305,6 +124,7 @@ vector<int> DetectCCT::DetectCCTsOnAPic()
 	cv::adaptiveThreshold(gray_img, binary_img, 1,
 		cv::ADAPTIVE_THRESH_MEAN_C, cv::THRESH_BINARY,
 		11, 2);
+	//提取轮廓
 	vector<vector<cv::Point>> contours;
 	cv::findContours(binary_img, contours,
 		cv::RETR_TREE,
@@ -319,13 +139,13 @@ vector<int> DetectCCT::DetectCCTsOnAPic()
 			);
 			double area = cv::contourArea(contours[i]);
 			double perimeter = cv::arcLength(contours[i], true);
-			double circularuty = (4.0 * CV_PI * area) 
+			double circularuty = (4.0 * CV_PI * area)
 				/ (perimeter * perimeter);
 			if (circularuty < 0.8) continue;
 			ellipse_rects.push_back(ellipse_rect);
 		}
 	}
-	//最里面的轮廓
+	//提取最里面的椭圆轮廓
 	vector<cv::RotatedRect> ellipse_rects_c1;
 	for (const auto& e : ellipse_rects)
 	{
@@ -333,37 +153,37 @@ vector<int> DetectCCT::DetectCCTsOnAPic()
 		{
 			double x = e.center.x - p.center.x;
 			double y = e.center.y - p.center.y;
-			if (sqrt(pow(x,2)+pow(y,2))<=5)
+			if (sqrt(pow(x, 2) + pow(y, 2)) <= 5)
 			{
 				ellipse_rects_c1.push_back(
-					(e.size.area() > p.size.area()) ? 
+					(e.size.area() > p.size.area()) ?
 					e : p
 				);
 			}
-		}		
+		}
 	}
-	//扩充轮廓，得到外面两层轮廓
+	//扩充轮廓，得到外面两层椭圆轮廓
 	vector<cv::RotatedRect> ellipse_rects_c2;
 	vector<cv::RotatedRect> ellipse_rects_c3;
 	for (const auto& e : ellipse_rects_c1)
 	{
-	ellipse_rects_c2.push_back(cv::RotatedRect(e.center,
+		ellipse_rects_c2.push_back(cv::RotatedRect(e.center,
 			cv::Size2f(e.size.width * 2, e.size.height * 2),
 			e.angle));
-	ellipse_rects_c3.push_back(cv::RotatedRect(e.center,
-		cv::Size2f(e.size.width * 4, e.size.height * 4),
-		e.angle));
+		ellipse_rects_c3.push_back(cv::RotatedRect(e.center,
+			cv::Size2f(e.size.width * 4, e.size.height * 4),
+			e.angle));
 	}
 	vector<cv::Mat> cct_imgs;//剪切图像
-	vector<cv::RotatedRect> ellipse_rects_c3_1;//剪切图像上的椭圆轮廓
-	for (const auto&e:ellipse_rects_c3)
+	vector<cv::RotatedRect> ellipse_rects_c3_1;//剪切后的图像上的椭圆轮廓
+	for (const auto& e : ellipse_rects_c3)
 	{
 		//剪切图像
 		cv::Rect bounding_rect = e.boundingRect();
 		cv::Mat cropped_img;
 		cv::getRectSubPix(color_img, bounding_rect.size(), e.center, cropped_img);
 		cct_imgs.push_back(cropped_img);
-		//剪切图像上的椭圆轮廓
+		//剪切后图像上的椭圆轮廓
 		cv::RotatedRect temp = e;
 		temp.center.x -= bounding_rect.x;
 		temp.center.y -= bounding_rect.y;
@@ -373,37 +193,41 @@ vector<int> DetectCCT::DetectCCTsOnAPic()
 	vector<int>result;
 	for (size_t i = 0; i < ellipse_rects_c3.size(); i++)
 	{
-		//仿射变换
-		cv::RotatedRect temp_rotate_rect = ellipse_rects_c3_1[i];
-		cv::Mat img = cct_imgs[i];
-		float major_axis = temp_rotate_rect.size.width / 2.0f;
-		float minor_axis = temp_rotate_rect.size.height / 2.0f;
-		float angle = temp_rotate_rect.angle;
-		cv::Point2f center = temp_rotate_rect.center;
-		float scale = major_axis / minor_axis;
-		cv::Mat rotation_matrix = cv::getRotationMatrix2D(
-			center,
-			-angle,
-			1/scale);
-		cv::Mat transformed_img;
-		cv::warpAffine(img, transformed_img, rotation_matrix, 
-			img.size());
-		//缩放
-		cv::Mat large_img;
-		cv::resize(transformed_img, large_img, cv::Size(200,200),cv::INTER_LANCZOS4);
-		//灰度化
-		cv::Mat gray_img;
-		cv::cvtColor(large_img,
-			gray_img,
-			cv::COLOR_BGR2GRAY);
-		//腐蚀
-		cv::Mat kernel = cv::getStructuringElement(
-			cv::MORPH_RECT, 
-			cv::Size(3, 3)
-		);
+		vector<int>temp;
 		cv::Mat eroded_img;
-		cv::erode(gray_img, eroded_img, kernel);
-		cct_imgs_after_tran.push_back(eroded_img);
+		//前处理剪切下的图像
+		{
+			//仿射变换
+			cv::RotatedRect temp_rotate_rect = ellipse_rects_c3_1[i];
+			cv::Mat img = cct_imgs[i];
+			float major_axis = temp_rotate_rect.size.width / 2.0f;
+			float minor_axis = temp_rotate_rect.size.height / 2.0f;
+			float angle = temp_rotate_rect.angle;
+			cv::Point2f center = temp_rotate_rect.center;
+			float scale = major_axis / minor_axis;
+			cv::Mat rotation_matrix = cv::getRotationMatrix2D(
+				center,
+				-angle,
+				1 / scale);
+			cv::Mat transformed_img;
+			cv::warpAffine(img, transformed_img, rotation_matrix,
+				img.size());
+			//缩放
+			cv::Mat large_img;
+			cv::resize(transformed_img, large_img, cv::Size(200, 200), cv::INTER_LANCZOS4);
+			//灰度化
+			cv::Mat gray_img;
+			cv::cvtColor(large_img,
+				gray_img,
+				cv::COLOR_BGR2GRAY);
+			//腐蚀
+			cv::Mat kernel = cv::getStructuringElement(
+				cv::MORPH_RECT,
+				cv::Size(3, 3)
+			);
+			cv::erode(gray_img, eroded_img, kernel);
+			cct_imgs_after_tran.push_back(eroded_img);
+		}
 		//采样解码
 		cv::Point cct_center(100, 100);
 		int unite_angle = 360 / detect_cct_info.N;
@@ -414,7 +238,7 @@ vector<int> DetectCCT::DetectCCTsOnAPic()
 			{
 				int row = round(
 					cct_center.y - i * sin(
-						(n* unite_angle+0.5*unite_angle)* PI / 180
+						(n * unite_angle + 0.5 * unite_angle) * PI / 180
 					)
 				);
 				int col = round(
@@ -422,7 +246,7 @@ vector<int> DetectCCT::DetectCCTsOnAPic()
 						(n * unite_angle + 0.5 * unite_angle) * PI / 180
 					)
 				);
-				uchar* data = eroded_img.ptr<uchar>(row); 
+				uchar* data = eroded_img.ptr<uchar>(row);
 				int intensity = data[col];
 				re_a.push_back(
 					intensity);
@@ -432,14 +256,198 @@ vector<int> DetectCCT::DetectCCTsOnAPic()
 				sum += r;
 			sum /= re_a.size();
 			(sum > 125) ? sum = 0 : sum = 1;
-			result.push_back(sum);
+			temp.push_back(sum);
 		}
+		reverse(temp.begin(), temp.end());
+		if (detect_cct_info.color == white)
+		{
+			for (size_t k = 0; k < temp.size(); k++)
+			{
+				(temp[k] == 1) ? temp[k] = 0 : temp[k] = 1;
+			}
+		}
+		//绘制
+		cv::ellipse(color_img, ellipse_rects_c1[i], 
+			cv::Scalar(0, 255, 0), 1);
+		cv::ellipse(color_img, ellipse_rects_c2[i],
+			cv::Scalar(0, 255, 0), 1);
+		cv::ellipse(color_img, ellipse_rects_c3[i],
+			cv::Scalar(0, 255, 0), 1);
+		int a_result = BinToInt(temp);
+		Text text(to_string(a_result), ellipse_rects_c1[i].center);
+		cv::putText(color_img, text.text, text.position, text.fontFace, text.fontScale, text.color, text.thickness, text.lineType);
+		result.push_back(BinToInt(temp));
 	}
-	reverse(result.begin(), result.end());
+	cv::imwrite(detect_cct_info.file_path, color_img);
 	return result;
 }
 
-int DetectCCT::GetInt(const vector<int>& bin_result)
+int BinToInt(const vector<int>& bin)
 {
-	return DrawCCT::BinToInt(bin_result,detect_cct_info.N);
+	int N = bin.size();
+	int num = 0;
+	for (int i : bin)
+	{
+		num += i * int(pow(2, N - 1));
+		N -= 1;
+	}
+	return num;
+}
+
+Text::Text(const string& text_, 
+	const cv::Point& position_):
+	text(text_),position(position_)
+{
+}
+
+Text::Text(const Text& text_)
+{
+	this->text = text_.text;
+	this->position = text_.position;
+}
+
+Text::Text()
+{
+	this->position = cv::Point(0, 0);
+	this->text = "";
+}
+
+ACCTInfo::ACCTInfo(const int& num_, const int& N_, const CCTColor& color_, const int& size_):
+	num(num_),N(N_),color(color_),size(size_)
+{
+}
+
+ACCTInfo::ACCTInfo(const ACCTInfo& acct_info_)
+{
+	this->color = acct_info_.color;
+	this->N = acct_info_.N;
+	this->num = acct_info_.num;
+	this->size = acct_info_.size;
+}
+
+ACCTInfo::ACCTInfo()
+{
+	this->color = black;
+	this->N = 0;
+	this->num = 0;
+	this->size = 0;
+}
+
+CCTInfo::CCTInfo(const int& N_, const CCTColor& color_, const int& size_):
+	N(N_),color(color_),size(size_)
+{
+}
+
+CCTInfo::CCTInfo(const CCTInfo& cct_info_)
+{
+	this->color = cct_info_.color;
+	this->N = cct_info_.N;
+	this->size = cct_info_.size;
+}
+
+CCTInfo::CCTInfo()
+{
+	this->color = black;
+	this->N = 0;
+	this->size = 0;
+}
+
+Circle::Circle(const cv::Point& center_, const int& radius_, const int& thickness_, const cv::Scalar& color_):
+	center(center_),radius(radius_),thickness(thickness_),color(color_)
+{
+}
+
+vector<int> IntToBin(const int& num_,const int&N)
+{
+	vector<int> bin;
+	int num = num_;
+	for (size_t i = 0; i < N; i++)
+	{
+		if (num > 0)
+		{
+			bin.push_back(num % 2);
+			num = num / 2;
+		}
+		else
+		{
+			bin.push_back(0);
+		}
+	}
+	reverse(bin.begin(), bin.end());
+	return bin;
+}
+
+Flabellate::Flabellate(const cv::Point& center_, const int& radius_, const int& thickness_, const cv::Scalar color_):
+	center(center_),radius(radius_),thickness(thickness_),color(color_)
+{
+}
+
+vector<cv::Mat> DrawCCTs(const CCTInfo& cct_info)
+{
+	//计算该位数可以表示的最大的数字
+	vector<int> temp;
+	for (size_t i = 0; i < cct_info.N; i++)
+		temp.push_back(1);
+	int max = BinToInt(temp);
+	//开始绘制
+	vector<cv::Mat> ccts;
+	for (size_t i = 0; i < max; i++)
+	{
+		ACCTInfo acct_info(i, cct_info.N, cct_info.color, cct_info.size);
+		ccts.push_back(DrawACCT(acct_info));
+	}
+	return ccts;
+}
+
+cv::Mat DrawCCTsOnAPic(const CCTInfo& cct_info)
+{
+	vector<cv::Mat> ccts = DrawCCTs(cct_info);
+	int pic_rows = int(sqrt(ccts.size())) + 1;
+	int pic_cols = pic_rows;
+	int pic_width = pic_cols * cct_info.size;
+	int pic_height = pic_rows * cct_info.size;
+	cv::Mat merged_img(pic_height, pic_width,
+		ccts[0].type());
+	int offsetX = 0;
+	int offsetY = 0;
+	for (const auto& img : ccts)
+	{
+		img.copyTo(merged_img(cv::Rect(offsetX, offsetY,
+			img.cols, img.rows)));
+		offsetX += img.cols;
+		if (offsetX >= pic_width)
+		{
+			offsetY += img.rows;
+			offsetX = 0;
+		}
+	}
+	string file_path = "";
+	string dir_path = "";
+	switch (cct_info.color)
+	{
+	case black:
+		file_path = string("E:/source/repos/main/CCT_Black_") + to_string(cct_info.N) + string("_MeragedImg / ") +
+			to_string(ccts.size()) + string(".png");
+		dir_path = string("E:/source/repos/main/CCT_Black_") + to_string(cct_info.N) + string("_MeragedImg / ");
+		break;
+	case white:
+		file_path = string("E:/source/repos/main/CCT_White_") + to_string(cct_info.N) + string("_MeragedImg / ") +
+			to_string(ccts.size()) + string(".png");
+		dir_path = string("E:/source/repos/main/CCT_White_") + to_string(cct_info.N) + string("_MeragedImg / ");
+		break;
+	default:
+		break;
+	}
+	//创建文件夹
+	if (!fs::exists(dir_path))
+	{
+		if (!fs::create_directory(dir_path))
+		{
+			cerr << "创建文件夹：" << dir_path <<
+				"失败！" << endl;
+			return cv::Mat();
+		}
+	}
+	cv::imwrite(file_path, merged_img);
+	return merged_img;
 }
--- CCTLib.h
+++ CCTLib.h
@@ -1,5 +1,5 @@
 #pragma once
-#define PI 3.1415926
+#define PI 3.1415926535
 
 #include<iostream>
 #include<string>
@@ -17,16 +17,30 @@ enum CCTColor
 	black = 1,
 	white = 0	
 };
+//文本
+struct Text
+{
+	string text;//文本
+	cv::Point position; // 文本位置
+	int fontFace = cv::FONT_HERSHEY_SIMPLEX; // 字体类型
+	double fontScale = 1.5; // 字体大小
+	cv::Scalar color = cv::Scalar(0,255,0); // 文本颜色 (BGR格式)
+	int thickness = 2; // 文本线宽
+	int lineType = cv::LINE_AA; // 文本线型
+public:
+	Text(const string& text_, const cv::Point& position_);
+	Text(const Text& text_);
+	Text();
+};
 //识别环形码所需的信息
 struct DetectCCTInfo
 {
-	string dir_path;//环形码所在的文件夹路径
-	vector<string> img_file_paths;//路径下所有图片文件路径
+	string file_path;//环形码路径
 	CCTColor color;//环形码的颜色
 	int N;//编码位数
 public:
-	void Init();
-	DetectCCTInfo(const string& dir_path_, const CCTColor& color_ , const int&N_);
+	DetectCCTInfo(const string& file_path_, const CCTColor& color_ , const int&N_);
+	DetectCCTInfo(const DetectCCTInfo&detect_cct_info_);
 	DetectCCTInfo();
 };
 //扇形
@@ -34,10 +48,10 @@ struct Flabellate
 {
 	cv::Point center;//圆心
 	int radius;//半径
-	double start_angle;//起始角度
-	double end_angle;//终止角度
 	int thickness = -1;//厚度，-1表示填充
 	cv::Scalar color;//颜色(BGR顺序)
+public:
+	Flabellate(const cv::Point& center_, const int& radius_, const int& thickness_, const cv::Scalar color_);
 };
 //圆
 struct Circle
@@ -46,55 +60,41 @@ struct Circle
 	int radius;//半径
 	int thickness = -1;//厚度
 	cv::Scalar color;//颜色
+public:
+	Circle(const cv::Point& center_, const int& radius_, const int& thickness_, const cv::Scalar& color_);
 };
-//绘制环形码所需的信息
+//绘制一个环形码所需的信息
+struct ACCTInfo
+{
+	int num;//编码的整数
+	int N;//编码位数
+	CCTColor color;//编码的颜色
+	int size;//图片尺寸
+public:
+	ACCTInfo(const int& num_, const int& N_, const CCTColor& color_, const int& size_);
+	ACCTInfo(const ACCTInfo& acct_info_);
+	ACCTInfo();
+};
+//绘制一批环形码所需的信息
 struct CCTInfo
 {
-	string dir_path;//文件夹路径
 	int N;//等分数
 	CCTColor color;//环形码颜色
 	int size;//图片尺寸
-	int num;//编码的整数
-	int max_num;//N位二进制表示的最大整数
-	vector<int> bin;//编码的二进制vector
-	Flabellate flabellate;//扇形信息
-	Circle circle_out;//外圆形（大）信息
-	Circle circle_in;//内圆形（小）信息
-
 public:
-	void Init();
-	CCTInfo(const int&N_,const CCTColor&color_,const int&size_,const int&num);
+	CCTInfo(const int& N_, const CCTColor& color_, const int& size_);
+	CCTInfo(const CCTInfo& cct_info_);
 	CCTInfo();
 };
 
-class DrawCCT
-{
-public:
-	DrawCCT(const CCTInfo& cct_info);
-	~DrawCCT();
-	//将整数转成二进制vector
-	vector<int> IntToBin( int num,const int& N);
-	//将二进制的vector转成整数
-	static int BinToInt(const vector<int>& bin, int N);
-	//绘制一张CCT图像
-	cv::Mat DrawACCT();
-	void DrawaCCT();
-	vector<cv::Mat> DrawcCTs();
-	void DrawCCTs();
-	void DrawCCTsOnAPic();
-private:
-	CCTInfo cct_infor;
-};
-class DetectCCT
-{
-public:
-	DetectCCT(const DetectCCTInfo& detect_cct_info_);
-	~DetectCCT();
-	vector<int> DetectCCTsOnAPic();
-	int GetInt(const vector<int>&bin_result);
-private:
-	DetectCCTInfo detect_cct_info;
-};
+vector<int> DecodeCCT(const DetectCCTInfo& detect_cct_info);
 
+int BinToInt(const vector<int>& bin);
 
+cv::Mat DrawACCT(const ACCTInfo& acct_info);
 
+vector<cv::Mat> DrawCCTs(const CCTInfo& cct_info);
+
+vector<int> IntToBin(const int& num_,const int&N);
+
+cv::Mat DrawCCTsOnAPic(const CCTInfo& cct_info);
